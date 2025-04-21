18c34cdcb1b7495127f1ae2353d9bff2aaefdda2
glazenbol <5520120+glazenbol@users.noreply.github.com>
2025-04-22 00:56:49 +0200

    move away from p5, plain html shi

--- public/sketch.js
+++ public/sketch.js
@@ -1,76 +1,36 @@
+
 let socket;
 let notes = []; // Store note positions
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(240);
+const noteElem = document.getElementById("note");
 
-    // Connect to the server
-    socket = io();
+noteElem.innerHTML = "Testerino"
+createCanvas(windowWidth, windowHeight-100);
+background(240);
 
-    button = [];
-    for (let i = 0; i < 5; i++) {
-        let but = createButton(i + 1);
-        but.mousePressed(function() {
-            player = i + 1
-            socket.emit('playerSelect', i)
-        });
-        but.position(windowWidth / 2, (i + 0.5) * (windowHeight / 5));
-        button.push();
-    }
+// Connect to the server
+socket = io();
 
-    // Listen for updates from other clients
-    socket.on('scoreUpdate', (data) => {
-        notes.push(data); // Add received note
+button = [];
+for (let i = 0; i < 5; i++) {
+    let but = createButton(i + 1);
+    but.mousePressed(function() {
+        socket.emit('playerSelect', i)
     });
+    but.position(windowWidth / 2, (i + 0.5) * (windowHeight / 5));
+    button.push();
 }
+
+// Listen for updates from other clients
+socket.on('scoreUpdate', (data) => {
+    notes.push(data); // Add received note
+});
+
+socket.on('note', (data) => {
+    console.log(data);
+    console.log(Tonal.Note.fromMidiSharps(data));
+    noteElem.innerHTML = data
+});
 
 
 let player = -1;
-
-function draw() {
-    if (player < 0) {
-        return;
-    }
-
-    background(240);
-
-    // Draw all notes
-    for (let note of notes) {
-        fill(0);
-        ellipse(note.x, note.y, 20, 20);
-    }
-}
-
-// Keep canvas same resolution as screen
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
-function mousePressed() {
-    let newNote = { x: mouseX, y: mouseY };
-    notes.push(newNote); // Add locally
-
-    socket.emit('scoreUpdate', newNote); // Send to others
-
-    enableWakeLock(); // Activate wake lock
-}
-
-let wakeLock = null;
-
-async function enableWakeLock() {
-    if ('wakeLock' in navigator) {
-        try {
-            wakeLock = await navigator.wakeLock.request('screen');
-            console.log("Wake Lock enabled");
-
-            // Auto-reacquire if it gets released
-            wakeLock.addEventListener('release', () => {
-                console.log('Wake Lock released, trying again...');
-                enableWakeLock();
-            });
-        } catch (err) {
-            console.error('Wake Lock error:', err);
-        }
-    }
-}
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+import express from 'express';
+import http from 'http';
+import  { Server } from 'socket.io';
+import { Note } from "tonal";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const players = [];
+let chordNumber = 0;
+const chords = [
+    {'chord': ['C#3', 'D#3', 'G#3'], upper: [['D4','E4']]},
+    {'chord': ['C#3', 'E3', 'B3'], upper: [['G#4', 'A4']]},
+    {'chord': ['C3', 'E3', 'D4'], upper: [['G4', 'B4'], ['A4', 'B4']]},
+    {'chord': ['A2', 'C#3', 'G#3'], upper: [['A#4', 'D#5'], ['D#5', 'E5']]},
+    {'chord': ['D#3', 'E3', 'G#3'], upper: [['F#4', 'D#5'], ['E4', 'C#5'], ['F#4', 'B4']]},
+]
 
 // Serve static files (for the client)
 app.use(express.static('public'));
@@ -12,7 +22,11 @@ app.use(express.static('public'));
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
     socket.on('playerSelect', (data) => {
-        console.log('Player selected:', data)
+        console.log('Player selected:', data);
+        players[data] = socket;
+        for (const pl of players) {
+            console.log(pl);
+        }
     });
 
     // Listen for score updates from a client
@@ -26,4 +40,64 @@ io.on('connection', (socket) => {
     });
 });
 
+let state = 0;
+let playersRandomized = players;
+let n_playing = 2;
+
+const changeState = function() {
+    console.log(players.length);
+    switch (state) {
+        case 0:
+            io.emit("state", "mute");
+            break;
+        case 1:
+            playersRandomized = shuffled(players);
+            for (let i = 0; i < n_playing; i ++) {
+                let note = chords[chordNumber].upper[0][i];
+                // protection for when not all players have connected yet TODO make start function
+                if (playersRandomized.length > i) {
+                    let player = playersRandomized[i];
+                    console.log("note emit");
+                    player.emit("note", Note.midi(note));
+                }
+                console.log("note:", note);
+            }
+            break;
+        case 2:
+            for (let i = n_playing; i < 5; i ++) {
+                let note = chords[chordNumber].chord[i];
+                // protection for when not all players have connected yet TODO make start function
+                if (playersRandomized.length > i) {
+                    let player = playersRandomized[i];
+                    console.log("note emit");
+                    player.emit("note", Note.midi(note));
+                }
+                console.log("note", note);
+            }
+            break;
+        case 3:
+            break;
+        case 4:
+            io.emit("state", "mute");
+            chordNumber ++;
+            chordNumber %= 5;
+            console.log("next chord:", chordNumber)
+            break;
+        default:
+            break;
+    }
+    state ++;
+    state %= 5;
+}
+
+function shuffled(arr) {
+    let shuffled = [...arr]; // Copy the array
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap
+    }
+    return shuffled;
+}
+
 server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+setInterval(changeState, 1000);//5000); TODO
